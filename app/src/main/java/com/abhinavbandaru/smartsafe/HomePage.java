aad25e3878713c179aa2d4fee2a52181aede6db9
Abhinav Bandaru <b.abhinav2000@gmail.com>
2020-09-25 05:59:34 +0530

    Implemented SOS button with emergency text to Rishi and continuous siren.For WhatsApp need to press send manually, hence rejected. Ignore UI

--- app/src/main/java/com/abhinavbandaru/smartsafe/HomePage.java
+++ app/src/main/java/com/abhinavbandaru/smartsafe/HomePage.java
@@ -1,7 +1,13 @@
 package com.abhinavbandaru.smartsafe;
 
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -18,11 +24,18 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
+
 public class HomePage extends AppCompatActivity {
     Button googleSignOutButton;
+    Button panicButton;
+    Button stopPanicButton;
     GoogleSignInClient mGoogleSignInClient;
     TextView googleName;
     TextView googleEmail;
+    AudioManager audioManager;
+    MediaPlayer mediaPlayer;
+    String emergencyContact;
+    private Handler handler;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,8 +46,25 @@ public class HomePage extends AppCompatActivity {
                 .build();
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
+        mediaPlayer = MediaPlayer.create(this, R.raw.siren);
+        audioManager = (AudioManager)getSystemService(AUDIO_SERVICE);
         googleName = findViewById(R.id.google_name);
         googleEmail = findViewById(R.id.google_email);
+        panicButton = findViewById(R.id.panic_button);
+        emergencyContact = "9989300201";
+        panicButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                panicRunnable.run();
+            }
+        });
+        stopPanicButton = findViewById(R.id.stop_panic_button);
+        stopPanicButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handler.removeCallbacks(panicRunnable);
+            }
+        });
         googleSignOutButton = findViewById(R.id.sign_out_button);
         googleSignOutButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,4 +99,27 @@ public class HomePage extends AppCompatActivity {
                     }
                 });
     }
+
+    private Runnable panicRunnable = new Runnable() {
+        @Override
+        public void run() {
+            audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC), 0);
+            mediaPlayer.start();
+            Intent smsIntent = new Intent(Intent.ACTION_VIEW);
+
+            smsIntent.setDataAndType(Uri.parse("smsto:"),"vnd.android-dir/mms-sms");
+            smsIntent.putExtra("address"  , new String (emergencyContact));
+            smsIntent.putExtra("sms_body"  , "Test ");
+
+            try {
+                startActivity(smsIntent);
+                finish();
+                Log.i("Finished sending SMS...", "");
+            } catch (android.content.ActivityNotFoundException ex) {
+                Toast.makeText(HomePage.this,
+                        "SMS faild, please try again later.", Toast.LENGTH_SHORT).show();
+            }
+            handler.postDelayed(panicRunnable,9000);
+        }
+    };
 }
